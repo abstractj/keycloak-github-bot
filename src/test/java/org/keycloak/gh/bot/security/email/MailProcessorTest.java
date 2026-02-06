d8d65143c706f83829979123dc7ad2fdb9a9143a
Bruno Oliveira da Silva <bruno@abstractj.com>
2026-02-06 06:27:25 -0300

    Optimize imports
    Signed-off-by: Bruno Oliveira da Silva <bruno@abstractj.com>

--- src/test/java/org/keycloak/gh/bot/security/email/MailProcessorTest.java
+++ src/test/java/org/keycloak/gh/bot/security/email/MailProcessorTest.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
