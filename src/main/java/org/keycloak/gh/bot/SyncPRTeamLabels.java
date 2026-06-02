59474d756be9e9420e3eeeb29a692d0235865f24
stianst <stianst@gmail.com>
2026-06-02 08:23:31 +0200

    Update SyncPRTeamLabels
    Signed-off-by: stianst <stianst@gmail.com>

--- src/main/java/org/keycloak/gh/bot/SyncPRTeamLabels.java
+++ src/main/java/org/keycloak/gh/bot/SyncPRTeamLabels.java
@@ -7,42 +7,55 @@ import org.kohsuke.github.GHEventPayload;
 import org.kohsuke.github.GHIssue;
 import org.kohsuke.github.GHLabel;
 import org.kohsuke.github.GHPullRequest;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 
 public class SyncPRTeamLabels {
 
     private static final Logger logger = Logger.getLogger(SyncPRTeamLabels.class);
-    private static final org.slf4j.Logger log = LoggerFactory.getLogger(SyncPRTeamLabels.class);
 
     void onOpen(@PullRequest.Opened GHEventPayload.PullRequest payload) throws IOException {
         GHPullRequest pullRequest = payload.getPullRequest();
 
+        List<String> pullRequestTeamLabels = getTeamLabels(pullRequest.getLabels());
+        if (!pullRequestTeamLabels.isEmpty()) {
+            logger.infof("New PR %s: Team labels %s already exists", pullRequestTeamLabels);
+            return;
+        }
+
         List<GHIssue> linkedIssues = CommitUtils.linkedIssues(payload.getRepository(), pullRequest);
         List<String> teamLabels = linkedIssues.stream().flatMap(i -> i.getLabels().stream()).map(GHLabel::getName).filter(l -> l.startsWith("team/")).distinct().toList();
+        List<Integer> linkedIssueNumbers = linkedIssues.stream().map(GHIssue::getNumber).toList();
 
         if (!teamLabels.isEmpty()) {
-            logger.infov("PR {0}: Adding labels {0} from issues {1}", pullRequest.getNumber(), teamLabels, linkedIssues.stream().map(GHIssue::getNumber).toList());
+            logger.infof("New PR %s: Adding labels %s from issues %s", pullRequest.getNumber(), String.join(", ", teamLabels), linkedIssueNumbers);
             pullRequest.addLabels(teamLabels.toArray(new String[0]));
         } else if (!linkedIssues.isEmpty()) {
-            logger.infov("PR {0}: No team labels found in issues {1}", pullRequest.getNumber(), linkedIssues.stream().map(GHIssue::getNumber).toList());
+            logger.infof("New PR %s: No team labels found in issues %s", pullRequest.getNumber(), linkedIssueNumbers);
         } else {
-            logger.infov("PR {0}: No linked issues found", pullRequest.getNumber());
+            logger.infof("New PR %s: No linked issues found", pullRequest.getNumber());
         }
     }
 
     void onLabelled(@PullRequest.Labeled GHEventPayload.PullRequest payload) throws IOException {
-        GHPullRequest pullRequest = payload.getPullRequest();
+        String newLabel = payload.getLabel().getName();
+        if (newLabel.startsWith("team/")) {
+            GHPullRequest pullRequest = payload.getPullRequest();
+            List<GHIssue> linkedIssues = CommitUtils.linkedIssues(payload.getRepository(), pullRequest);
+            List<Integer> linkedIssueNumbers = linkedIssues.stream().map(GHIssue::getNumber).toList();
 
-        String[] teamLabels = pullRequest.getLabels().stream().map(GHLabel::getName).filter(l -> l.startsWith("team/")).distinct().toArray(String[]::new);
-
-        List<GHIssue> linkedIssues = CommitUtils.linkedIssues(payload.getRepository(), pullRequest);
-        for (GHIssue linkedIssue : linkedIssues) {
-            logger.infov("Issue {0}: Adding labels {0} from issue {1}", linkedIssue.getNumber(), teamLabels, pullRequest.getNumber());
-            linkedIssue.addLabels(teamLabels);
+            if (!linkedIssues.isEmpty()) {
+                logger.infof("Team label %s added to PR %s: Updating issues %s", newLabel, pullRequest.getNumber(), linkedIssueNumbers);
+            } else {
+                logger.infof("Team label %s added to PR %s: No linked issues found", newLabel, pullRequest.getNumber());
+            }
         }
+    }
+
+    private List<String> getTeamLabels(Collection<GHLabel> labels) {
+        return labels.stream().map(GHLabel::getName).filter(l -> l.startsWith("team/")).toList();
     }
 
 }
